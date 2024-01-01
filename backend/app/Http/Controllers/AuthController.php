f23e8091f6bb11cac794c1bbd85f1540bc5bee49
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-01 12:06:09 +0200

    modify login api and the login functionality

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -42,32 +42,107 @@ class AuthController extends Controller
 
     }
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $request->validate([
             'first_name' => 'required|string|max:50',
             'last_name' => 'required|string|max:50',
             'email' => 'required|string|email|max:100|unique:users',
+            'user_type_id' => 'integer',
             'password' => 'required|string|min:6',
         ]);
-
+    
         $user = User::create([
-            'first_name' => $request->first_name,            
+            'first_name' => $request->first_name,
             'last_name' => $request->last_name,
+            'user_type_id' => $request->user_type_id ?? 1,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
-
+    
         $token = Auth::login($user);
+        
         return response()->json([
             'status' => 'success',
             'message' => 'User created successfully',
             'user' => $user,
-            'authorisation' => [
+            'authorization' => [
                 'token' => $token,
                 'type' => 'bearer',
             ]
         ]);
     }
+    
+
+    // public function register(Request $request){
+    //     $request->validate([
+    //         'first_name' => 'required|string|max:50',
+    //         'last_name' => 'required|string|max:50',
+    //         'password' => 'required|string|min:6',
+    //     ]);
+
+    //     $user = User::create([
+    //         'first_name' => $request->first_name,            
+    //         'last_name' => $request->last_name,
+    //         'email' => $request->email,
+    //         'password' => Hash::make($request->password),
+    //     ]);
+
+    //     $token = Auth::login($user);
+    //     return response()->json([
+    //         'status' => 'success',
+    //         'message' => 'User created successfully',
+    //         'user' => $user,
+    //         'authorisation' => [
+    //             'token' => $token,
+    //             'type' => 'bearer',
+    //         ]
+    //     ]);
+    // }
+
+    // public function register(Request $request)
+    // {
+    //     try {
+    //         $request->validate([
+    //             'first_name' => 'required|string|max:50',
+    //             'last_name' => 'required|string|max:50',
+    //             'email' => 'required|string|email|max:100|unique:users',
+    //             'password' => 'required|string|min:6',
+    //         ]);
+
+    //         $user = User::create([
+    //             'first_name' => $request->first_name,            
+    //             'last_name' => $request->last_name,
+    //             'email' => $request->email,
+    //             'password' => Hash::make($request->password),
+    //         ]);
+
+    //         $token = Auth::login($user);
+
+    //         return response()->json([
+    //             'status' => 'success',
+    //             'message' => 'User created successfully',
+    //             'user' => $user,
+    //             'authorization' => [
+    //                 'token' => $token,
+    //                 'type' => 'bearer',
+    //             ]
+    //         ]);
+
+    //     } catch (\Exception $e) {
+    //         // Log the exception or handle it in a way that makes sense for your application
+    //         // You might want to log the error, return a custom error response, etc.
+    //         // For now, let's log the error and return a generic error response.
+
+    //         \Log::error('Error during registration: ' . $e->getMessage());
+
+    //         return response()->json([
+    //             'status' => 'error',
+    //             'message' => 'An error occurred during registration. Please try again later.',
+    //             'error' => $e,
+    //         ], 500); // 500 Internal Server Error
+    //     }
+    // }
 
     public function logout()
     {
--- frontend/src/components/pages/auth/login/index.jsx
+++ frontend/src/components/pages/auth/login/index.jsx
@@ -1,41 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import TextInput from '../../../input';
 import CustomButton from '../../../button';
 import { Link } from 'react-router-dom';
+import { request } from '../../../../helpers/request';
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log("Login with:", email, password);
+    try {
+      const user = { email, password };
+      const response = await request('/login', 'POST', user);
+
+      console.log(response);
+
+      // Handle success, e.g., redirect to another page
+    } catch (error) {
+      console.error('Error during login:', error);
+      // Handle error, e.g., show an error message to the user
+    }
+  };
+
   return (
     <div className='login-page'>
-      <div className='right-side'>
-       
-      </div>
+      <div className='right-side'></div>
       <div className='left-side'>
-      <form className='form-data'>
+        <form className='form-data' onSubmit={handleLogin}>
           <h1 className='title'>Log in</h1>
           <div className="center">
-
-          <TextInput
-            label='Email'
-            placeholder='Enter your email'
-            type='email'
-          />
-
-          <TextInput
-            label='Password'
-            placeholder='Enter your password'
-            type='password'
-          />
-          <div className='btn'>
-            <CustomButton 
-            children={'Log in'}
-            className= "login-btn"
+            <TextInput
+              label='Email'
+              placeholder='Enter your email'
+              type='email'
+              onChange={(e) => setEmail(e.target.value)}
             />
 
-            <p className='mark'>Do not have an account? <Link to='/register' className='link-to'>
-                  Sign Up
-                </Link></p>
-          </div>
+            <TextInput
+              label='Password'
+              placeholder='Enter your password'
+              type='password'
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div className='btn'>
+              <CustomButton
+                children={'Log in'}
+                className="login-btn"
+                type="submit"
+              />
+
+              <p className='mark'>Do not have an account? <Link to='/register' className='link-to'>
+                Sign Up
+              </Link></p>
+            </div>
           </div>
         </form>
       </div>
@@ -44,3 +65,51 @@ const Login = () => {
 };
 
 export default Login;
+
+
+// import React from 'react';
+// import './index.css';
+// import TextInput from '../../../input';
+// import CustomButton from '../../../button';
+// import { Link } from 'react-router-dom';
+
+// const Login = () => {
+//   return (
+//     <div className='login-page'>
+//       <div className='right-side'>
+       
+//       </div>
+//       <div className='left-side'>
+//       <form className='form-data'>
+//           <h1 className='title'>Log in</h1>
+//           <div className="center">
+
+//           <TextInput
+//             label='Email'
+//             placeholder='Enter your email'
+//             type='email'
+//           />
+
+//           <TextInput
+//             label='Password'
+//             placeholder='Enter your password'
+//             type='password'
+//           />
+//           <div className='btn'>
+//             <CustomButton 
+//             children={'Log in'}
+//             className= "login-btn"
+//             />
+
+//             <p className='mark'>Do not have an account? <Link to='/register' className='link-to'>
+//                   Sign Up
+//                 </Link></p>
+//           </div>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Login;
