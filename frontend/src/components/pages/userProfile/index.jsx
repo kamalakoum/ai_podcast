b593072d4da1c2beceeb77459bbb8b667dcc48d1
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-04 13:20:54 +0200

    add request update profile

--- frontend/src/components/pages/userProfile/index.jsx
+++ frontend/src/components/pages/userProfile/index.jsx
@@ -22,19 +22,32 @@ const UserProfile = () => {
             });
             console.log('Axios Config:', response.config);
             
-            // Set the state with user data
             setFirstName(response.first_name);
             setLastName(response.last_name);
             setEmail(response.email);
             setPassword(response.password);
-            console.log(response);
-            // console.log(response.first_name);
+
         } catch (error) {
             console.error('Error during fetching user details:', error);
         }
     };
+
+    const updateUserDetails = async (e) => {
+        e.preventDefault();
+        const token = localStorage.getItem('token');
+        try {
+          const user = {first_name,last_name,email,password};
+          const response = await request('/userprofile', 'POST', user, {
+            'Authorization': `Bearer ${token}`,
+          });
     
-    // Call getUserDetails when the component mounts
+          console.log(response);
+    
+        } catch (error) {
+          console.error('Error during registration:', error);
+        }
+    }
+    
     useEffect(() => {
         getUserDetails();
     }, []);
@@ -43,7 +56,7 @@ const UserProfile = () => {
   return (
     <div className='register-page'>
       <div className='form-container'>
-        <form className='form-data'>
+        <form className='form-data' onSubmit={updateUserDetails}>
           <h1 className='title'>User Profile</h1>
           <div className="center">
             <TextInput
@@ -52,7 +65,7 @@ const UserProfile = () => {
               type='text'
               name='first_name'
               value={first_name}
-            //   onChange={handleInputChange}
+              onChange={(e) => setFirstName(e.target.value)}
               required
             />
 
@@ -62,7 +75,7 @@ const UserProfile = () => {
               type='text'
               name='last_name'
               value={last_name}
-            //   onChange={handleInputChange}
+              onChange={(e) => setLastName(e.target.value)}
             />
 
             <TextInput
@@ -71,7 +84,7 @@ const UserProfile = () => {
               type='email'
               name='email'
               value={email}
-            //   onChange={handleInputChange}
+              onChange={(e) => setEmail(e.target.value)}
             />
 
             <TextInput
@@ -80,7 +93,7 @@ const UserProfile = () => {
               type='password'
               name='password'
               value={password}
-            //   onChange={}
+              onChange={(e) => setPassword(e.target.value)}
             />
 
             <div className='btn'>
@@ -102,3 +115,105 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+// import React, { useState, useEffect } from 'react';
+// import './index.css';
+// import TextInput from '../../input';
+// import CustomButton from '../../button';
+// import { Link } from 'react-router-dom';
+// import { request } from '../../../helpers/request';
+
+// const UserProfile = () => {
+
+//     const [first_name,setFirstName] = useState("");
+//     const [last_name,setLastName] = useState("");
+//     const [email,setEmail] = useState("");
+//     const [password,setPassword] = useState("");
+
+//     const getUserDetails = async () => {
+//         try {
+//             const token = localStorage.getItem('token');
+//             console.log(token);
+//             console.log('Authorization'+ `Bearer ${token}`,)
+//             const response = await request('/userprofile', 'GET', null, {
+//                     'Authorization': `Bearer ${token}`,
+//             });
+//             console.log('Axios Config:', response.config);
+            
+//             setFirstName(response.first_name);
+//             setLastName(response.last_name);
+//             setEmail(response.email);
+//             setPassword(response.password);
+
+//         } catch (error) {
+//             console.error('Error during fetching user details:', error);
+//         }
+//     };
+    
+//     useEffect(() => {
+//         getUserDetails();
+//     }, []);
+    
+  
+//   return (
+//     <div className='register-page'>
+//       <div className='form-container'>
+//         <form className='form-data'>
+//           <h1 className='title'>User Profile</h1>
+//           <div className="center">
+//             <TextInput
+//               label='First Name'
+//               placeholder={first_name}
+//               type='text'
+//               name='first_name'
+//               value={first_name}
+//             //   onChange={}
+//               required
+//             />
+
+//             <TextInput
+//               label='Last Name'
+//               placeholder={last_name}
+//               type='text'
+//               name='last_name'
+//               value={last_name}
+//             //   onChange={}
+//             />
+
+//             <TextInput
+//               label='Email'
+//               placeholder={email}
+//               type='email'
+//               name='email'
+//               value={email}
+//             //   onChange={}
+//             />
+
+//             <TextInput
+//               label='Password'
+//               placeholder={password}
+//               type='password'
+//               name='password'
+//               value={password}
+//             //   onChange={}
+//             />
+
+//             <div className='btn'>
+//               <CustomButton
+//                 children={'Update Profile'}
+//                 className="update-btn"
+//                 type="submit"
+//               />
+//               <p className='mark'>Want to logout? <Link to='/' className='link-to'>
+//                 Log out
+//               </Link></p>
+//             </div>
+//           </div>
+//         </form>
+//       </div>
+//       <div className='right-reg'></div>
+//     </div>
+//   );
+// };
+
+// export default UserProfile;
