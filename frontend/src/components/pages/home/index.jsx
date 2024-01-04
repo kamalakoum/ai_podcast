419d5770d36a1735c51aa219cc28c8a30eca4e5f
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-04 14:37:11 +0200

    adding navbar

--- frontend/src/components/pages/home/index.jsx
+++ frontend/src/components/pages/home/index.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import './index.css';
+import CustomButton from "../../button";
 
 const Home = () => {
 
       
     return (
       <div className='home-page'>
+        <header>
+            <div className="navbar">
+                <CustomButton
+                    children={'Profile'}
+                    className="home-btn"
+                    type="submit"
+                />
+
+                <CustomButton
+                    children={'Logout'}
+                    className="home-btn"
+                    type="submit"
+                 />
+            </div>
+        </header>
        <h1>Podcastify</h1>
        <div>
         <p>Transform Your RSS Feed into Personalized Podcasts</p>
