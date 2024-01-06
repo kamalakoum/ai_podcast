8ac9aa81370a00f6766c05acc00408f1f16580b6
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-06 22:33:49 +0200

    admin: add request

---
+++ admin-dashboard/src/helpers/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+
+const request = async (route, method = "POST" , data, headers) => {
+    console.log(route,method,data , headers);
+    try {
+        const response = await axios.request({
+            url:route,
+            method: method,
+            withCredentials:false,
+            data:data,
+            headers :{
+                "Content-Type" : "application/json",
+                ...headers,
+            }
+        
+        });
+        return response.data;
+    }catch(e){  
+        console.log("error in API Request",e);
+    }
+};
+
+export {request}
