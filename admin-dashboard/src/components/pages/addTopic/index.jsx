59c27def8eb9e46574bb6715ffa4fa913f32c002
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-07 14:25:28 +0200

    admin: add container to hold form in addtopics

--- admin-dashboard/src/components/pages/addTopic/index.jsx
+++ admin-dashboard/src/components/pages/addTopic/index.jsx
@@ -18,6 +18,10 @@ const AddTopics = () => {
                     </ul>
                 </nav>
             </div>
+            
+            <div>
+                
+            </div>
 
         </div>
     );
