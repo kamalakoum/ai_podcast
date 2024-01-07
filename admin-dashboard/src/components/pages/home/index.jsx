a820b5c295c01af016994af7c34f5c1f46738427
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-07 04:38:24 +0200

    admin: add style to navbar

--- admin-dashboard/src/components/pages/home/index.jsx
+++ admin-dashboard/src/components/pages/home/index.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import './index.css';
 
 const Home = () => {
     return (
         <div className="admin-home">
             <nav className="navbar">
-                <ul>
-                    <li>
+                <ul className="list-element">
+                    <li className="element">
                         <a href="/add-topics">Add Topics</a>
                     </li>
-                    <li>
+                    <li className="element">
                         <a href="/add-rss-feed">Add RSS Feed</a>
                     </li>
                 </ul>
             </nav>
-            {/* Other content of the Home component */}
         </div>
     );
 };
