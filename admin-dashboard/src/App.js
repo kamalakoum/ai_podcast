c890159dd46a816c39ddf0745c3d6517c7846f6c
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-07 13:17:03 +0200

    admin: import BrowserRouter,Routes, Route

--- admin-dashboard/src/App.js
+++ admin-dashboard/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/pages/login';
 import Home from './components/pages/home';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
