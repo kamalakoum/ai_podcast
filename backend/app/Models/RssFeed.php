b9e091d464bb0cf48d6e814e2fbbeba37a97f9a4
Kamal Akoum <kamal.akoun@anghami.com>
2024-01-14 16:42:25 +0200

    backend: add fillablearray to RssFeed Model

--- backend/app/Models/RssFeed.php
+++ backend/app/Models/RssFeed.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class RssFeed extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['source', 'url', 'topic_id'];
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
+    
 }
